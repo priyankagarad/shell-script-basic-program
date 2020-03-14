d8f0f95fe94616e34788c8b6d2ce012210f9910b
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-14 10:36:32 +0530

    Added:display unit of number

--- case statement/unitCase.sh
+++ case statement/unitCase.sh
@@ -2,12 +2,12 @@
 read -p "enter the a number:" num
 echo $num
 case $num in 
-1)
-	echo "unit" ;;
-10)
-	echo "ten" ;;
-100)
-	echo "hundred" ;;
-1000)
-	echo "thousand" ;;
+	1)
+		echo "unit" ;;
+	10)
+		echo "ten" ;;
+	100)
+		echo "hundred" ;;
+	1000)
+		echo "thousand" ;;
 esac

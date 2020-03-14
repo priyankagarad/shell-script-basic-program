b791b35cfa63055f66f607b3d095f734603dd3f3
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-14 10:12:12 +0530

    Added:display day of week

---
+++ case statement/digitWord.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+result=$((RANDOM %10))
+echo $result
+case "$result" in
+0)
+	echo "Zero" ;;
+1)
+	echo "first" ;;
+2)
+	echo "second" ;;
+3)
+	echo "three" ;;
+4)
+	echo "fourth" ;;
+5)
+	echo "five" ;;
+6)
+	echo "six" ;;
+7)
+	echo "seven" ;;
+8)
+	echo "eight" ;;
+9)
+	echo "nine" ;;
+esac

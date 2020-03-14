4897f2a66da22242328abdbf54f02540e0eefdd1
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-14 09:11:46 +0530

    display day of week

---
+++ Selection else if statements/dayOfWeek.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+read -p "enter a number:" num
+$num
+
+if [ $num -eq 1 ]
+then 
+	echo "sunday"
+elif [ $num -eq 2 ]
+then
+	echo "monday"
+
+elif [ $num -eq 3 ]
+then 
+	echo "tuesday"
+
+elif [ $num -eq 4 ]
+then 
+	echo "wednesday"
+
+elif [ $num -eq 5 ]
+then 
+	echo "thursday"
+
+elif [ $num -eq 6 ]
+then 
+	echo "friday"
+
+elif [ $num -eq 7 ]
+then 
+	echo "saturday"
+else
+
+	echo "number not found"
+fi

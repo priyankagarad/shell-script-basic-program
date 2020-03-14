11849a6503d8364cb8be2b2670ea7cce34bc4c26
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-14 09:13:21 +0530

    display single digit number in word

---
+++ Selection else if statements/singleDigit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+read -p "enter a number" num
+$num
+
+if [ $num -eq 1 ]
+then
+	echo "One"
+
+elif [ $num -eq 2 ]
+then 
+	echo "two" 
+
+elif [ $num -eq 3 ]
+then 
+	echo "three"
+
+elif [ $num -eq 4 ]
+then 
+	echo "four"
+
+
+elif [ $num -eq 5 ]
+then 
+        echo "five"
+
+
+elif [ $num -eq 6 ]
+then 
+        echo "six"
+
+elif [ $num -eq 7 ]
+then 
+        echo "seven"
+
+elif [ $num -eq 8 ]
+then 
+        echo "eight"
+
+elif [ $num -eq 9 ]
+then 
+        echo "nine"
+
+fi
+
+
+
+

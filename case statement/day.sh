8628fd8c063c66a1ba3cff108c682e945fd25bf7
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-14 10:34:08 +0530

    Added:display day of week

--- case statement/day.sh
+++ case statement/day.sh
@@ -2,18 +2,18 @@
 read -p "enter a number:" num
 echo $num
 case "$num" in
-0)
-	echo "sunday" ;;
-1)
-	echo "monday" ;;
-2)
-	echo "tuesday" ;;
-3)
-	echo "wednesday" ;;
-4)
-	echo "thursday";;
-5)
-	echo "friday" ;;
-6)
-	echo "saturday" ;;
+	0)
+		echo "sunday" ;;
+	1)
+		echo "monday" ;;
+	2)
+		echo "tuesday" ;;
+	3)
+		echo "wednesday" ;;
+	4)
+		echo "thursday";;
+	5)
+		echo "friday" ;;
+	6)
+		echo "saturday" ;;
 esac
